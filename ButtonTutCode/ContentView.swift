046a4991b302a3031f619da56c8309de76149955
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-29 08:16:04 +0200

    /.2

--- ButtonTutCode/ContentView.swift
+++ ButtonTutCode/ContentView.swift
@@ -17,17 +17,30 @@ struct ContentView: View {
             Button("Button #1") {
                 self.title = "Button #1 was pressed!"
             }
+            .accentColor(.red)
             Button {
                 self.title = "Button #2 was pressed!"
             } label: {
                 Text("Button #2")
             }
+            .accentColor(.orange)
             Button(action: {
                 self.title = "Button #3 was pressed!"
             }, label: {
-                Text("Button #3")
+                Text("Button #3".uppercased())
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal, 20)
+                    .background(
+                        Color.blue
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                    )
                 
-            })
+            }
+            )
 
         }
     }
