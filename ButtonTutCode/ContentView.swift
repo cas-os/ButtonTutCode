c8d615a8a5ee506dcb9ff1920d3f7ef35340aae8
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-29 08:09:52 +0200

    /.1

--- ButtonTutCode/ContentView.swift
+++ ButtonTutCode/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var title: String = "Bonsoir Elliot"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 30) {
+            Text(title)
+            Button("Button #1") {
+                self.title = "Button #1 was pressed!"
+            }
+            Button {
+                self.title = "Button #2 was pressed!"
+            } label: {
+                Text("Button #2")
+            }
+            Button(action: {
+                self.title = "Button #3 was pressed!"
+            }, label: {
+                Text("Button #3")
+                
+            })
+
+        }
     }
 }
 
