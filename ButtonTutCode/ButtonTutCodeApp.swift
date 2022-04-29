81978c54a4789ea73f921ec18e556343db0f484d
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-29 07:56:26 +0200

    Initial Commit

---
+++ ButtonTutCode/ButtonTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTutCodeApp.swift
+//  ButtonTutCode
+//
+//  Created by HAL-9001 on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ButtonTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
